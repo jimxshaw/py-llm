143a62f86069aefb761a3dd835d0fb48fd31ffa4
Jim Shaw <jimxshaw@gmail.com>
2023-12-22 18:26:33 -0600

    Update text boxes.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import gradio as gradio
 
 with gradio.Blocks() as server:
-  gradio.Textbox(label="Input", value="Default value...")
+  with gradio.Tab("LLM Inferencing"):
+    llm_input = gradio.Textbox(label="Question:", value="Input question here...", interactive=True)
+    llm_output = gradio.Textbox(label="Answer:", interactive=False, value="Output answer here...")
 
 server.launch()
