9ba9c78bd21b5ae2da9c4dafff19ca441d2fb846
Jim Shaw <jimxshaw@gmail.com>
2023-12-28 11:49:54 -0600

    Add openai api.

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import gradio as gradio
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
+
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def ask(text):
-  return text.upper()
+  completion = openai_client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{
+      "role": "user",
+      "content": text
+    }],
+    temperature=0
+  )
+
+  return completion.choices[0].message.content
 
 with gradio.Blocks() as server:
   with gradio.Tab("LLM Inferencing"):
